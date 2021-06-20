6ff740670c204b694edb599f7be2effdcbebe2c0
marcelo.azevedo <marcelo.azevedo@connectcorp.com.br>
2021-06-20 15:40:18 -0300

    payments with Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,42 @@
-import  styles from './styles.module.scss';
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
+import styles from './styles.module.scss';
 
 interface SubscribeProps {
   priceId: string;
 }
 
-export function SubscribeButton({}: SubscribeProps) {
+export function SubscribeButton({ priceId }: SubscribeProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github')
+      return;
+    }
+
+    // create checkout session
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+      
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   );
